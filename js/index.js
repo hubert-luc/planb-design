8f7ce641c4b271cbdf002c60d8e5204f830f8019
Hubert Łuc <h.luc@global4net.com>
2021-10-22 10:49:52 +0200

    progress

--- js/index.js
+++ js/index.js
@@ -1,13 +1,72 @@
+// set up text to print, each item in array is new line
+var workshopTextArray = new Array(
+    "Pracownia"
+    );
+    var contactTextArray = new Array(
+        "Kontakt"
+        );
+        var projectsTextArray = new Array(
+            "Projekty"
+            );
+    var iSpeed = 70; // time delay of print out
+    var iScrollAt = 20; // start scrolling up at this many lines
+  
+    initialiseTextPositions = {
+        "workshop-link": 0,
+        "contact-link": 0,
+        "projects-link": 0
+    }
 
+    const typewriter = (textArray, elementId) => {
+        var iIndex = 0; // start printing array at this posision
+        var iArrLength = textArray[0].length; // the length of the text array
+        var sContents = ''; // initialise contents variable
+        var iRow; // initialise current row
+        sContents =  ' ';
+        iRow = Math.max(0, iIndex-iScrollAt);
+        var destination = document.getElementById(elementId);
+         
+         while ( iRow < iIndex ) {
+          sContents += textArray[iRow++] + '<br />';
+         }
+         destination.innerHTML = sContents + textArray[iIndex].substring(0, initialiseTextPositions[elementId]);
+         if ( initialiseTextPositions[elementId]++ == iArrLength ) {
+            initialiseTextPositions[elementId] = 0;
+          iIndex++;
+          if ( iIndex != textArray.length ) {
+           iArrLength = textArray[iIndex].length;
+           setTimeout(() => typewriter(textArray, elementId), 500);
+          }
+         } else {
+          setTimeout(() => typewriter(textArray, elementId), iSpeed);
+         }
+    }
+
+setTimeout(() => {
+    typewriter(projectsTextArray, 'projects-link');
+}, 18 * iSpeed)
+
+setTimeout(() => {
+    typewriter(workshopTextArray, 'workshop-link');
+}, 28 * iSpeed)
+
+setTimeout(() => {
+typewriter(contactTextArray, 'contact-link');
+}, 39 * iSpeed)
+
+projectsLink = document.getElementById('projects-link');
+projectsLink.addEventListener('click', () => {
+    window.scrollTo({top: window.innerHeight, left: 0, behavior: 'smooth'});
+})
 
 fetch("https://api.npoint.io/e0e65b68b7b40a71357f").then(response => {
     return response.json()
 }).then(data => {
     const projects = data.projects;
-console.log(projects)
     const list = document.getElementById('projects-list');
 
     Object.keys(projects).forEach(name => {
+        console.log(name)
     const project = projects[name];
     const projectPreview = document.createElement('a');
     projectPreview.href = `/project.html?name=${name}`
@@ -29,8 +88,12 @@ console.log(projects)
 
     previewImageContainer.appendChild(previewImage);
     projectPreview.appendChild(previewImageContainer);
-    projectPreview.appendChild(previewTitle);
-    projectPreview.appendChild(previewShortDescription)
+    const title = document.createElement('span');
+    title.className = 'title';
+    title.innerHTML = project.name;
+    previewImageContainer.appendChild(title);
+    // projectPreview.appendChild(previewTitle);
+    // projectPreview.appendChild(previewShortDescription)
     const listItem = document.createElement('li');
     listItem.className = 'project-preview';
     listItem.appendChild(projectPreview);
--- js/project.js
+++ js/project.js
@@ -1,9 +1,34 @@
 const projectName = window.location.search.split('=')[1];
-
+const body = document.getElementById('container')
+console.log(body)
 fetch("https://api.npoint.io/e0e65b68b7b40a71357f").then(response => {
     return response.json()
 }).then(data => {
     const project = data.projects[projectName]
-    console.log(project)
+
+    const title = document.getElementById('title');
+    title.className = 'title-container';
+    const name = document.createElement('h1');
+    name.textContent = project.name;
+    name.className = 'name';
+    title.appendChild(name)
     
+    const shortDescription = document.createElement('p');
+    shortDescription.textContent = project.shortDescription;
+    shortDescription.className = 'short-description';
+    title.appendChild(shortDescription);
+
+    // setTimeout(() => {
+    //     window.scrollTo({top: window.innerHeight, left: 0, behavior: 'smooth'});
+    // }, 4000)
+
+    const imagesArray = JSON.parse(project.images);
+    imagesArray.forEach(imageUrl => {
+        console.log(imageUrl)
+        const image = document.createElement('img');
+        image.src = imageUrl;
+        console.log(image)
+        body.appendChild(image)
+    })
+
 })
